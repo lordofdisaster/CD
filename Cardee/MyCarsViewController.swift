97567e1a6020aa28c65222fd42055a13aafff2e9
leo mac <leonif1@gmail.com>
2017-07-10 22:17:56 +0300

    convert My Cars to Storyboard

--- Cardee/MyCarsViewController.swift
+++ Cardee/MyCarsViewController.swift
@@ -17,37 +17,25 @@ class MyCarsViewController: CardeeViewController {
     var cars = [SimpleCar]()
     var refreshControl: UIRefreshControl!
     let CarCellIdentifier = "CarCellIdentifier"
+    @IBOutlet weak var carsTableViews: UITableView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.title = "My Cars"
-        
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add Car", style: .plain, target: self, action: #selector(self.addCar))
+        configureView()
         
         MBProgressHUD.showAdded(to: (self.navigationController?.view)!, animated: true)
         AlamofireManager.getOwnerProfile() { success, error in
-            MBProgressHUD.hide(for: (self.navigationController?.view)!, animated: true)
+            
             self.cars.removeAll()
             self.cars = OwnerProfile.shared.cars
-            self.tableView.reloadData()
+            self.carsTableViews.reloadData()
+            
+            MBProgressHUD.hide(for: (self.navigationController?.view)!, animated: true)
         }
         
-        self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: Screen.width, height: Screen.height - CGFloat(System.tabBarHeight) - CGFloat(System.navigationBarHeight) - CGFloat(System.statusBarHeight)))
-        self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 13, 0)
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        self.tableView.register(CarTableViewCell.self, forCellReuseIdentifier: CarCellIdentifier)
-        self.tableView.backgroundColor = Color.lightGray
-        self.tableView.tableFooterView = UIView()
-        self.tableView.separatorStyle = .none
-        self.view.addSubview(self.tableView)
         
-        self.refreshControl = UIRefreshControl()
-        self.refreshControl.attributedTitle = NSAttributedString(string: "Refresh...")
-        self.refreshControl.addTarget(self, action: #selector(self.refresh(_:)), for: .valueChanged)
-        self.tableView.refreshControl = self.refreshControl
     }
     
     func refresh(_ sender: UIRefreshControl) {
@@ -55,15 +43,36 @@ class MyCarsViewController: CardeeViewController {
             self.refreshControl.endRefreshing()
             self.cars.removeAll()
             self.cars = OwnerProfile.shared.cars
-            self.tableView.reloadData()
+            self.carsTableViews.reloadData()
         }
     }
     
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    func configureView() {
+        //self.title = "My Cars"
+        //self.tableView.delegate = self
+        //self.tableView.dataSource = self
+        
+        
+        //self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add Car", style: .plain, target: self, action: #selector(self.addCar))
+        
+        
+        //self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: Screen.width, height: Screen.height - CGFloat(System.tabBarHeight) - CGFloat(System.navigationBarHeight) - CGFloat(System.statusBarHeight)))
+        //self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 13, 0)
+        //self.tableView.register(CarTableViewCell.self, forCellReuseIdentifier: CarCellIdentifier)
+//        self.tableView.backgroundColor = Color.lightGray
+//        self.tableView.tableFooterView = UIView()
+//        self.tableView.separatorStyle = .none
+//        self.view.addSubview(self.tableView)
+        
+        
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.attributedTitle = NSAttributedString(string: "Refresh...")
+        self.refreshControl.addTarget(self, action: #selector(self.refresh(_:)), for: .valueChanged)
+        self.carsTableViews.refreshControl = self.refreshControl
+        
     }
+    
+    
     
     //MARK: Actions
     
@@ -99,9 +108,9 @@ extension MyCarsViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: CarCellIdentifier, for: indexPath) as? CarTableViewCell {
+        if let cell = carsTableViews.dequeueReusableCell(withIdentifier: CarCellIdentifier, for: indexPath) as? CarTableViewCell {
             let car = cars[indexPath.row]
-            cell.configureCell(car: car)
+            cell.configureCell(car)
     
             return cell
         }
